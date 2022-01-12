3214f13d9b7d5a076ca09c2ccf5ef9452c8fc9ec
Jean-Sébastien <jssteux@osivia.com>
2022-01-12 12:49:29 +0100

    add proxy

--- docker/portal/entrypoint.sh
+++ docker/portal/entrypoint.sh
@@ -1,5 +1,17 @@
 #!/bin/bash
 set -e
 
-catalina.sh jpda run
+# Portal properties
+PORTAL_PROPERTIES=/portal.properties
+
+#PRONOTE Secrets
+PUBLIC_HOST=${PUBLIC_HOST:-localhost}
+
+
+echo "Adapting portal.properties"
+
+sed -i s\\PUBLIC_HOST\\$PUBLIC_HOST\\g $PORTAL_PROPERTIES
+
+
+/usr/local/tomcat/bin/catalina.sh jpda run
 
