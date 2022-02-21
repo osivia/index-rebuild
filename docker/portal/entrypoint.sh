d3baef99ec9666a355f68afaf360b500a1f6f90b
Jean-Sébastien <jssteux@osivia.com>
2022-02-21 12:41:13 +0100

    docker cluster configuration

--- docker/portal/entrypoint.sh
+++ docker/portal/entrypoint.sh
@@ -7,12 +7,14 @@ PORTAL_PROPERTIES=/portal.properties
 
 PUBLIC_HOST=${PUBLIC_HOST:-localhost}
 MAIL_PWD=${MAIL_PWD:-}
+ROUTE_ID=${ROUTE_ID:-portal-1}
 
 
 echo "Adapting portal.properties"
 
 sed -i s\\PUBLIC_HOST\\$PUBLIC_HOST\\g $PORTAL_PROPERTIES
 sed -i s\\MAIL_PWD\\$MAIL_PWD\\g $PORTAL_PROPERTIES
+sed -i s\\ROUTE_ID\\$ROUTE_ID\\g $PORTAL_PROPERTIES
 
 /usr/local/tomcat/bin/catalina.sh jpda run
 
